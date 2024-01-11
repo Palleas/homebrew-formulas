692084955e2a3b5ebccb2db300543668ab00e8b1
Romain Pouclet <romain.pouclet@gmail.com>
2024-01-11 18:25:08 +0000

    Update formula for 0.1.13

--- agamotto.rb
+++ agamotto.rb
@@ -1,8 +1,8 @@
 class Agamotto < Formula
   desc "CLI tool to check out of date packages"
   homepage "https://romain.codes"
-  url "https://palleas-storage.s3.us-west-1.amazonaws.com/homebrew/agamotto-0.1.12.tar.gz"
-  sha256 "2d78f9b2d46a985deb0ce2297aaea1d7e330a3bcf6a15b7fb1823be62fec2694"
+  url "https://palleas-storage.s3.us-west-1.amazonaws.com/homebrew/agamotto-0.1.13.tar.gz"
+  sha256 "2d90e95a2ea1134436638a68b9697249b9d127a19d4fc4cf1fdad56503e8abea"
 
   depends_on "jq"
 
