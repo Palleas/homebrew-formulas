fb72f86ecda6cd72ced6f829ced4268c352f5514
Romain Pouclet <code@romain-pouclet.com>
2024-07-09 20:26:39 -0700

    Release agamotto 0.1.28

--- agamotto.rb
+++ agamotto.rb
@@ -1,13 +1,12 @@
 class Agamotto < Formula
   desc "CLI tool to check out of date packages"
   homepage "https://romain.codes"
-  url "https://palleas-storage.s3.us-west-1.amazonaws.com/homebrew/agamotto-0.1.13.tar.gz"
-  sha256 "2d90e95a2ea1134436638a68b9697249b9d127a19d4fc4cf1fdad56503e8abea"
-
+  url "https://github.com/Palleas/Agamotto/releases/download/0.1.28/agamotto.tar.gz"
+  sha256 "93b9dde0e07332a06cce0f5f98f5e67e866f7b95951dcb4b34bcd6e49a525ea4"
+  version "0.1.28"
   depends_on "jq"
 
   def install
-    bin.install "Agamotto_SwiftPackageManager.bundle"
     bin.install "agamotto"
   end
 
